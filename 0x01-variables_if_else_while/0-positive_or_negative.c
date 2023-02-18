f6a78fcbb15c18e0c573ce8532ac21c410e6c83c
Alimi Bolaji <alimi.fuhad@gmail.com>
2023-02-17 21:52:58 -0800

    Todays work 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+/* betty style doc files  function goes there */
+/**
+ * main - This program assigned a random number to the variable n
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;
+		/* your code goes there */
+		if (n > 0)
+		{
+		printf("%d is %s\n", n, "positive");
+		}
+		else if (n < 0)
+		{
+		printf("%d is %s\n", n, "negative");
+		}
+		else
+		{
+		printf("%d is %s\n", n, "zero");
+		}
+		return (0);
+}
